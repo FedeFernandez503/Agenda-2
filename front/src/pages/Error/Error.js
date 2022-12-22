c97456734966160a43ae9f736de93305954deb28
FedeFernandez503 <80352692+FedeFernandez503@users.noreply.github.com>
2022-12-22 00:27:45 -0300

    Update Error.js
    update error

--- front/src/pages/Error/Error.js
+++ front/src/pages/Error/Error.js
@@ -1,4 +1,5 @@
 import "./Error.css"
+import { Link } from "react-router-dom"
 
 export function Error() {
     return (
@@ -6,6 +7,9 @@ export function Error() {
             <h1 id="titleError">Error 404 not found</h1>
             <p>Please enter a valid url</p>
             <img id="imageError" src="https://images.pexels.com/photos/704555/pexels-photo-704555.jpeg?auto=compress&cs=tinysrgb&w=1600"></img>
+            <Link to="/" className="link">
+                Home
+            </Link>
         </div>
     )
 }
